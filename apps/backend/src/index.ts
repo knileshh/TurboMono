86259836d4ebb40aaaa610fb70ce9600f5b4f9ba
knilesh9 <neutron_stays@protonmail.com>
2024-05-29 12:23:11 +0530

    Used esbuild TS compiler to get around the export error and now we're done with the basics

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+import { BACKEND_URL } from "@repo/common/config"
+
+console.log(BACKEND_URL)
+
 app.get("/", (req, res) => {
     res.json({
         message: "Hello Turbo"
